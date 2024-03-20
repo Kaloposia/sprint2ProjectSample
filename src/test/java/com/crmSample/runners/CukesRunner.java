9d72de971ba9d713095eb0fd40a231c7ba3b8bf4
kaloposia <kalopsiacraft@gmail.com>
2024-03-20 22:03:16 +0000

    Setup

---
+++ src/test/java/com/crmSample/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.crmSample.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
+        },
+        features = "src/test/resources/features",
+        glue = "com/crmSample/step_definitions",
+        dryRun = false,
+        tags = "@wip",
+        publish = true //generating a report with public link
+)
+public class CukesRunner {}
+
+
